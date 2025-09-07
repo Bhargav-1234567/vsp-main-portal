0a15e57779e156df845b367b4ac3cbb35a0afd39
Bhargav <bhargavgadhiya601@gmail.com>
2025-09-07 13:28:55 +0530

    fix:adde UI change for slider and header section

--- src/components/InfinityRippleButton.jsx
+++ src/components/InfinityRippleButton.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function InfinityRippleButton({}) {
   // merge user style but set the CSS var for size
 
   return (
-    <button className="ripple-button">
-      Inquiry Now
-      <div className="ripple-outer ripple-outer-1"></div>
-      <div className="ripple-outer ripple-outer-2"></div>
-      <div className="ripple-outer ripple-outer-3"></div>
-    </button>
+    <Link to={"/contact"}>
+      <button className="ripple-button">
+        Inquiry Now
+        <div className="ripple-outer ripple-outer-1"></div>
+        <div className="ripple-outer ripple-outer-2"></div>
+        <div className="ripple-outer ripple-outer-3"></div>
+      </button>
+    </Link>
   );
 }
